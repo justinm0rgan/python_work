494324a4321485108d74e29996d9de045f6961a4
Justin Williams <justinwilliams@macbook-pro.home>
2020-03-23 09:39:25 -0400

    sitka rainfall issue

--- chp16/csv/sika_highs.py
+++ chp16/csv/sika_highs.py
@@ -1,29 +1,28 @@
 import csv
-
-import matplotlib.pyplot as plt
 from datetime import datetime
+
+from matplotlib import pyplot as plt
 
 filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
-	reader = csv.reader(f)
-	header_row = next(reader)
+    reader = csv.reader(f)
+    header_row = next(reader)
 
-
-	# Get dates and high temperatures from this file.
-	dates, highs = [], []
-	for row in reader:
-		current_date = datetime.strptime(row[2], '%Y-%m-%d')
-		high =int(row[5])
-		dates.append(current_date)
-		highs.append(high)
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        dates.append(current_date)
+        high = int(row[5])
+        highs.append(high)
 
 # Plot the high temperatures.
-plt.style.use('seaborn-dark')
-fig, ax = plt.subplots(figsize=(15,9))
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
-# Format plot
-plt.title("Daily high termperatures - 2018", fontsize=24)
+# Format plot.
+plt.title("Daily high temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
