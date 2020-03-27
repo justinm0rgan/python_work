a38053602b4d3ec231862391eec8763eaa991402
Justin Williams <justinwilliams@macbook-pro.home>
2020-03-27 09:50:58 -0400

    automatic index working

--- chp16/csv/automatic_indexes.py
+++ chp16/csv/automatic_indexes.py
@@ -7,29 +7,28 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/death_valley_2018_simple.csv'
-with open(filename) as f:
+filename = 'data/sitka_weather_2018_simple'
+csv_filename = filename + '.csv'
+png_filename = filename + '.png'
+with open(csv_filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
 	# Get dates, high and low temperatures from this file.
 	dates, highs, lows = [], [], []
+	tmax = header_row.index('TMAX')
+	tmin = header_row.index('TMIN')
 	for row in reader:
 		current_date = datetime.strptime(row[2], '%Y-%m-%d')
-		if row == 'TMAX':
-			high = row
-		if row == 'TMIN':
-			low = row
-		# try:
-		# 	high = int(row['TMAX'].Data_value)
-		# 	low = int(row['TMIN'].Data_value)
-		# except ValueError:
-		# 	print(f"Missing data for {current_date}")
+		try:
+			high = int(row[tmax])
+			low = int(row[tmin])
+		except ValueError:
+			print(f"Missing data for {current_date}")
 		else:
 			dates.append(current_date)
 			highs.append(high)
 			lows.append(low)
-
 
 # Graphing code
 # Plot the high temperatures.
@@ -40,13 +39,13 @@ ax.plot(dates, lows, c='blue', alpha=0.6)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-title = (f"{row[2]}")
+title = (f"Station {row[0]} Temperature Variation")
 plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-ax.set_ylim(20,130)
+# ax.set_ylim(20,130)
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
-# plt.savefig('death_valley_2018_daily_hi_low_temp.png', bbox_inches='tight')
+plt.savefig(png_filename, bbox_inches='tight')
 plt.show()
