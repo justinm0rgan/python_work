c41c8f0cefc23f8cf0b0686cf4d82c578f395d6b
Justin Williams <justinwilliams@macbook-pro.home>
2020-04-22 08:02:50 -0400

    world fires solved

--- chp16/mapping_world_data_sets/world_fires.py
+++ chp16/mapping_world_data_sets/world_fires.py
@@ -6,37 +6,36 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'data/world_fires_7_day.csv'
+filename = 'data/MODIS_C6_Global_7d.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     # Get lat/lon and brightness from file and append to list
-    lons, lats, brights = [], [], []
+    lats, lons, brights = [], [], []
     for row in reader:
-            lons.append(float(row[1]))
             lats.append(float(row[0]))
+            lons.append(float(row[1]))
             brights.append(float(row[2]))
-    print(lons[:10])
 
-# # Map the world fires.
-# data = [{
-#     'type': 'scattergeo',
-#     'lon': lons,
-#     'lat': lats,
-#     # 'text': hover_texts,
-#     'marker': {
-#         'size': brights,
-#         'color': brights,
-#         'colorscale': 'Reds',
-#         'reversescale': False,
-#         'colorbar': {'title': 'Brightness'},
-#     },
-# }]
+# Map the world fires.
+data = [{
+    'type': 'scattergeo',
+    'lat': lats,
+    'lon': lons,
+    # 'text': hover_texts,
+    'marker': {
+        'size': 7,
+        'color': brights,
+        'colorscale': 'Reds',
+        'reversescale': False,
+        'colorbar': {'title': 'Brightness'},
+    },
+}]
 
-# # Automate the map title
-# title = "World Fires 7 day"
-# my_layout = Layout(title=title)
+# Automate the map title
+title = "World Fires 7 day"
+my_layout = Layout(title=title)
 
-# fig = {'data': data, 'layout': my_layout}
-# offline.plot(fig, filename='world_fires_7_day.html')
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='world_fires_7_day.html')
